6514c52b719935fbecf87c85ed334fa8e5e92b3e
Vlad-AndreiIonescu <rinoboyu14@gmail.com>
2022-03-02 17:35:14 +0200

    hello world project created

--- src/ro/ase/cts/gr1085/lab01/Main.java
+++ src/ro/ase/cts/gr1085/lab01/Main.java
@@ -3,17 +3,39 @@ package ro.ase.cts.gr1085.lab01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Locale;
+
+class LowerCaseException  extends  Exception{
+    public String getMessage(){
+        return "Au fost introduse doar litere mici";
+    }
+}
+
+class UpperCaseException extends Exception{
+    public String getMessage(){
+        return "Au fost introduse doar litere mari";
+    }
+}
+
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("testati ceva: ");
+        System.out.println("Tastati cu litere mari si mici: ");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         try {
             String text = bufferedReader.readLine();
-            System.out.println(text);
-        } catch (IOException e) {
-            e.printStackTrace();
+            //System.out.println(text);
+
+            //daca toate literele sunt mici(lowercase)- LowerCaseException
+            if(text.toLowerCase(Locale.ROOT).equals(text)) throw new LowerCaseException();
+            if(text.toUpperCase(Locale.ROOT).equals(text)) throw new UpperCaseException();
+            //daca toate literele sunt mari(uppercase)- UpperCaseException
+
+
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
---
+++ src/ro/ase/cts/gr1085/lab01/exceptions/UpperCase.java
@@ -0,0 +1,4 @@
+package ro.ase.cts.gr1085.lab01.exceptions;
+
+public class UpperCase {
+}
